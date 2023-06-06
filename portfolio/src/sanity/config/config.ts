f3794729338df31dfb57b057d75f0f81bf7e1dd3
alexfarias7 <alexfarias44@gmail.com>
2023-06-06 15:31:54 -0300

    feat:implemeting styles tailwind

--- portfolio/src/sanity/config/config.ts
+++ portfolio/src/sanity/config/config.ts
@@ -2,4 +2,5 @@ export const configSchema = {
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_DATASET,
   apiVersion: process.env.NEXT_PUBLIC_API_VERSION,
+  useCdn: false,
 };
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,18 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx, html}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blue0: "#E2E5F4",
+        blue1: "#C7CDE9",
+        blue2: "#959CBF",
+        blue3: "#5D6280",
+        blue4: "#282B3F",
+        blue5: "#1C1E2C",
+        blue6: "#141625",
+        blue7: "#0E0F1F",
+        blue8: "#080A19",
+        blue9: "#050615",
+
+        purple0: "#F1ECFE",
+        purple1: "#ECE8FD",
+        purple2: "#E4DAFE",
+        purple3: "#D4C7FC",
+        purple4: "#C7B8FA",
+        purple5: "#B2A0F7",
+        purple6: "#8674D4",
+        purple7: "#6050B1",
+        purple8: "#40338F",
+        purple9: "#291E76",
+      },
+      fontFamily: {
+        popins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        firamono: ["Fira Mono", "monospace"],
+      },
+      screens: {
+        xl: { max: "1200px" },
+        lg: { max: "992px" },
+        md: { max: "740px" },
+        sm: { max: "480px" },
+      },
+      backgroundSize: {
+        "size-200": "200% 200%",
+      },
+      backgroundPosition: {
+        "pos-0": "0% 0%",
+        "pos-100": "100% 100%",
       },
     },
   },
   plugins: [],
-}
+};
