673fc25b4de1832835f7ec18fe4436be051c0515
alexfarias7 <alexfarias44@gmail.com>
2023-08-16 05:13:06 -0300

    fix: fixing button hero section in home

--- portfolio/src/components/Buttons/BtnIcon/index.tsx
+++ portfolio/src/components/Buttons/BtnIcon/index.tsx
@@ -9,9 +9,9 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 const BtnIcon = ({ className, text, icon, ...props }: Props) => {
   return (
     <div className="relative">
-      <div className="bg-blue5 border-solid border-purple7 border-[1px] h-[50px] w-[220px]  rounded-sm left-2 absolute top-2 transition-all -z-10 " />
+      <div className="bg-blue5 border-solid border-purple7 border-[1px] h-[50px] w-[250px]  rounded-sm left-2 absolute top-2 transition-all -z-10 " />
       <button
-        className={cn("btn-icons w-[220px]  h-[50px]", className)}
+        className={cn("btn-icons w-[250px]  h-[50px]", className)}
         {...props}
       >
         <span>{text}</span>
